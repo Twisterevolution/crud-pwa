de92528ba1964c82bbead359e57942e05bc7bec8
Jonathan Prieto <jonathan.prieto@ESPEX.CL>
2020-10-08 00:45:51 -0300

    heroku server.js

---
+++ dist/precache-manifest.bda98515903086dd5407f228a7829fea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b248cee92ee3518e734b",
+    "url": "/css/about.17fae0b1.css"
+  },
+  {
+    "revision": "e26b0a4354167a4f5f3e",
+    "url": "/css/chunk-vendors.52b87104.css"
+  },
+  {
+    "revision": "608468f1b7471d6852235f93b11b84c5",
+    "url": "/img/logo.608468f1.png"
+  },
+  {
+    "revision": "6e4b92c19dadb886ec39efc499402007",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b248cee92ee3518e734b",
+    "url": "/js/about.3768902a.js"
+  },
+  {
+    "revision": "094aada0924f519f02be",
+    "url": "/js/app.9ccb7c5c.js"
+  },
+  {
+    "revision": "e26b0a4354167a4f5f3e",
+    "url": "/js/chunk-vendors.97c8d79d.js"
+  },
+  {
+    "revision": "102ab81d518903d815e146e1e6a706b0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
